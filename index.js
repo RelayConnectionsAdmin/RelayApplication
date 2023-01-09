77d742160d37694ac467aae12fe4d694a5ffb11c
RelayConnectionsAdmin <122250892+RelayConnectionsAdmin@users.noreply.github.com>
2023-01-09 08:10:33 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,13 @@
+const app = express();
+app.use(express.static(path.join(process.cwd() + "/public")));
+const PORT = process.env.PORT;
+const CONNECTION = process.env.MONGODB_CONNECTION;
+mongoose
+
+  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
@@ -12,26 +22,16 @@ import UploadRoute from "./routes/UploadRoute.js";
 import ChatRoute from "./routes/ChatRoute.js";
 import MessageRoute from "./routes/MessageRoute.js";
 
-const app = express();
-
 // middleware
 app.use(bodyParser.json({ limit: "30mb", extended: true }));
 app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 // to serve images inside public folder
 
-app.use(express.static(path.join(process.cwd() + "/public")));
 dotenv.config();
-const PORT = process.env.PORT;
+
 // app.use(express.static("/public"));
 app.use("/images", express.static("images"));
-
-const CONNECTION = process.env.MONGODB_CONNECTION;
-mongoose
-
-  .connect(CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => app.listen(PORT, () => console.log(`Listening at Port ${PORT}`)))
-  .catch((error) => console.log(`${error} did not connect`));
 
 app.use("/auth", AuthRoute);
 app.use("/user", UserRoute);
